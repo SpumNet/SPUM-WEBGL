728f60ecbcfd70d0b395ce563a7f2e91dd6a8f98
SpumNet <astrovus@soonsoons.com>
2024-10-24 19:18:53 +0900

    ds

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,5 +1,5 @@
 
-const CACHE_VERSION = '0.12_2024.10.24.19.12';
+const CACHE_VERSION = '0.12_2024.10.24.19.17';
 const CACHE_NAME = `game-cache-${CACHE_VERSION}`;
 
 const CACHE_TARGETS = [
--- version-info.js
+++ version-info.js
@@ -1,5 +1,5 @@
 
 window.UNITY_VERSION_INFO = {
     version: '0.12',
-    buildDate: '2024.10.24.19.12'
+    buildDate: '2024.10.24.19.17'
 };
