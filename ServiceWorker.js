1ee6f5fa63a3258f3a165007c448b1cdb9de4ad3
SpumNet <astrovus@soonsoons.com>
2024-10-24 19:13:56 +0900

    ca

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,67 @@
-const cacheName = "DefaultCompany-PixelEditor-0.12";
-const contentToCache = [
-    "Build/WEB.loader.js",
-    "Build/WEB.framework.js",
-    "Build/WEB.data",
-    "Build/WEB.wasm",
-    "TemplateData/style.css"
 
+const CACHE_VERSION = '0.12_2024.10.24.19.12';
+const CACHE_NAME = `game-cache-${CACHE_VERSION}`;
+
+const CACHE_TARGETS = [
+    'index.html',
+    'Build/*.js',
+    'Build/*.wasm',
+    'Build/*.data',
+    'Build/*.framework.js',
+    'TemplateData/*'
 ];
 
-self.addEventListener('install', function (e) {
-    console.log('[Service Worker] Install');
+self.addEventListener('install', event => {
+    console.log('[ServiceWorker] Installing version:', CACHE_VERSION);
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(name => {
+                    if (name !== CACHE_NAME) {
+                        console.log('[ServiceWorker] Deleting old cache:', name);
+                        return caches.delete(name);
+                    }
+                })
+            );
+        }).then(() => {
+            return caches.open(CACHE_NAME).then(cache => {
+                console.log('[ServiceWorker] Creating new cache:', CACHE_NAME);
+                return cache.addAll(CACHE_TARGETS);
+            });
+        })
+    );
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
+self.addEventListener('activate', event => {
+    console.log('[ServiceWorker] Activated');
+    event.waitUntil(self.clients.claim());
+});
 
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request)
+            .then(response => {
+                // framework.js는 항상 네트워크에서 가져오기
+                if (event.request.url.includes('.framework.js')) {
+                    return response;
+                }
+                
+                const clonedResponse = response.clone();
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, clonedResponse);
+                });
+                
+                return response;
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
+});
+
+self.addEventListener('message', event => {
+    if (event.data === 'skipWaiting') {
+        self.skipWaiting();
+    }
 });
---
+++ version-info.js
@@ -0,0 +1,5 @@
+
+window.UNITY_VERSION_INFO = {
+    version: '0.12',
+    buildDate: '2024.10.24.19.12'
+};
