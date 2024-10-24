5df7cc8274df6a491dba296047ae08a95079d04f
SpumNet <astrovus@soonsoons.com>
2024-10-24 16:35:16 +0900

    s

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-PixelEditor-0.1";
+const contentToCache = [
+    "Build/WEB.loader.js",
+    "Build/WEB.framework.js",
+    "Build/WEB.data",
+    "Build/WEB.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
